d86d924e5f8239a479948f4d86d25abe64bd0e5d
liutong <liutong880818@163.com>
2021-02-03 22:18:58 +0800

    添加文件

---
+++ Java_git/src/main/java/com/java/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.java.git;
+
+/**
+ * Description: 描述
+ * Date: 2021/2/3 22:11
+ * author: LT
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
